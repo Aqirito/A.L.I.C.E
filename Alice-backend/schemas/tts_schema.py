e029a9e732a9e7b45332e22d626d5b495a552fd3
Aqirito <aqil.rahmat99@mail.com>
2025-01-23 17:34:09 +0800

    Update tts settings schema

--- Alice-backend/schemas/tts_schema.py
+++ Alice-backend/schemas/tts_schema.py
@@ -1,11 +1,19 @@
 from pydantic import BaseModel
 
 class BaseTtsModel(BaseModel):
-  tts_type: str
-  language: str
-  speed: float
-  speaker_id: int
-  voice: str
+  """
+  'voice': 'en-US-AnaNeural',
+  'pitch': '+0Hz',
+  'rate':'+0%',
+  'volume': '+0%'
+  EdgeTTS or MoeTTS
+  """
+  tts_type: str              # EdgeTTS, MoeTTS
+  volume: str
+  language: str              # [EN], [JA], [ZH], [KO] (MoeTTS)
+  speed: float               # good results is 0.77
+  speaker_id: int            # 607 (MoeTTS)
+  voice: str                 # copy and paste one name in a list of voices from models/TTS/moe-tts-speaker.txt
   pitch: str
   rate: str
   volume: str
