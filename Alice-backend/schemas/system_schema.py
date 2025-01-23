e27b52cebdc4d2443ccacf9788fa709661c89122
Aqirito <aqil.rahmat99@mail.com>
2025-01-23 17:34:20 +0800

    update system schema

--- Alice-backend/schemas/system_schema.py
+++ Alice-backend/schemas/system_schema.py
@@ -2,8 +2,4 @@ from pydantic import BaseModel
 
 class SystemSchema(BaseModel):
     template_type: str         # for now is 'pygmalion' and 'prompt'
-    model_type: str            # GPTQ
     model_loader: str          # AutoGPTQ, HuggingFaceBig, ExLlama
-    language: str              # [EN], [JA], [ZH], [KO]
-    speed: float               # good results is 0.77
-    speaker_id: int            # 607
