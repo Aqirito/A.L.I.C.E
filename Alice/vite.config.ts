6b067d8663694b7823a4099d11ddcc3157b6ac39
Aqirito <aqil.rahmat99@mail.com>
2025-01-29 02:45:40 +0800

    change from live2d to mmd

--- Alice/vite.config.ts
+++ Alice/vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+  optimizeDeps: {
+    exclude: ['babylon-mmd', '@babylonjs/havok']
+  },
+  build: {
+    rollupOptions: {
+      external: ['babylon-mmd']
+    }
+  }
 });
